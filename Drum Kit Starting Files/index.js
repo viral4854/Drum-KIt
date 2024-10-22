8cf5fa8123457f6df80f4778ec95a8030873d27c
viral4854 <contact.vdedaniya@gmail.com>
2024-10-23 02:24:49 +0530

    Initial commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,58 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        //this.style.color = "white";
+        // var audio = new Audio("sounds/tom-1.mp3");
+        // audio.play();
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                
+                break;
+
+            case "a":
+                var kickBass = new Audio("sounds/kick-bass.mp3");
+                kickBass.play();
+        
+                break;
+
+            case "s":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+        
+                break;
+
+            case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+        
+                break;
+
+            case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+        
+                break;
+
+            case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+        
+                break;
+
+            case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+        
+                break;
+        }
+
+    });
+}
+
